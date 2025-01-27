4e40a4e3384095b9eced4fd3dafbf76c2dc4d6d5
sarah <sarah.vogels@aol.de>
2025-01-27 11:55:09 +0100

    changes github workflow and minor changes

--- backend/tests/protzilla/data_integration/test_enrichment_analysis.py
+++ backend/tests/protzilla/data_integration/test_enrichment_analysis.py
@@ -29,7 +29,7 @@ from backend.protzilla.data_integration.enrichment_analysis_gsea import (
     gsea_preranked,
     create_ranked_df,
 )
-from backend.protzilla.data_integration.database_query import is_biomart_available #TODO S update ronjas commit
+from backend.protzilla.data_integration.database_query import is_biomart_available
 
 # isort:end_skip_file
 
