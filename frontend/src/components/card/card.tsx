14de8f09383e6e4ebf306392ea44b3b1a6044166
maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
2025-02-12 12:03:30 +0100

    minor changes on cards and switch toggle

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { CardProps } from "./card.props";
+import { spacing } from "../../theme";
+
 
 const StyledCard = styled.div`
   background: white;
-  border-radius: 8px;
+  border-radius: 8px; 
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  padding: 16px;
-`;
+  padding: ${spacing("small")};
+`; //${border("defaultRadius")}
 
 const CardBody = styled.div`
-  padding: 8px 0;
+  padding: ${spacing("small")} 0;
 `;
 
 const CardTitle = styled.h5`
   font-size: 1.25rem;
-  margin-bottom: 12px;
+  margin-bottom: ${spacing("small")};
 `;
 
 export const Card: React.FC<CardProps> = ({ title, children, className }) => {
