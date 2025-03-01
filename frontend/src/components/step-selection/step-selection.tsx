3361cb9dc4f5c83ac74e48102be2b14ce4edcf91
sarah <sarah.vogels@aol.de>
2025-03-01 13:45:18 +0100

    added CORS configurations (on working branch)

--- frontend/src/components/step-selection/step-selection.tsx
+++ frontend/src/components/step-selection/step-selection.tsx
@@ -18,7 +18,7 @@ export const StepSelection: React.FC<StepSelectionProps> = ({
     const fetchList = async () => {
       try {
         const response = await fetch(
-          "http://127.0.0.1:8000" + "/api/step_name_list/",
+          "http://127.0.0.1:8000/api/step_name_list/",
         );
         setDebug("meep: ");
 
