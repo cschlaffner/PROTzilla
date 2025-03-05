999d035ac7a0458c384bc7cb42ed406f97205f24
ronjakrg <ronja.krueger@student.hpi.de>
2025-03-05 13:03:01 +0100

    add mockup screen for table

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -8,6 +8,7 @@ import { getTheme, GlobalStyles, ThemeProvider } from "../theme";
 import { AutomaticErrorNotification } from "./automatic-error-notification";
 import { setupRootStore, StoreProvider } from "./store";
 import { CountersScreen, IndexScreen } from "../screens";
+import TableScreen from "../screens/table-screen";
 
 function App() {
   const [isReady, setIsReady] = useState(false);
@@ -37,6 +38,7 @@ function App() {
               <Route path="/" element={<MainScreen />}>
                 <Route path="/" element={<IndexScreen />} />
                 <Route path="/counters" element={<CountersScreen />} />
+                <Route path="/table" element={<TableScreen />} />
               </Route>
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
