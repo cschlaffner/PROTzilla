875a3c8b57490db8fb61d0faf41827cb327565ea
Philipp Hendel <philipp_c@hendel.co>
2025-03-12 14:51:59 +0100

    add spaces

--- frontend/src/components/sidebar/sidebar-section/sidebar-section.props.ts
+++ frontend/src/components/sidebar/sidebar-section/sidebar-section.props.ts
@@ -2,9 +2,9 @@ import { SectionNames, SelectedStep, SetSelectedStep } from "../types";
 
 export interface SidebarSectionProps extends React.HTMLAttributes<HTMLDivElement>{
     name: SectionNames;
-    index:number;
-    title:string;
-    isCollapsed:boolean;
-    selectedStep:SelectedStep;
+    index: number;
+    title: string;
+    isCollapsed: boolean;
+    selectedStep: SelectedStep;
     setSelectedStep: SetSelectedStep;
 }
