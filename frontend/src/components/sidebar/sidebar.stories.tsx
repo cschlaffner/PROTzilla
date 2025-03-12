02ca1f6c56633c56ea42570f35497ed5c1eeeeb2
Philipp Hendel <75376639+pchklm@users.noreply.github.com>
2025-03-12 14:59:38 +0100

    add Sidebar component
    * starting setup of new repo
    
    * bare minimum setup to copy PROTzilla2
    
    * new structure with empty backend-frontend
    
    * minor changes
    
    * added empty react project
    
    * added react workshop structure
    
    * more workshop content and pnpm commands
    
    * added .github configurations for backend and frontend, added lint- and other precommit hooks
    
    * Update Readmes
    
    * added more setup files from PROTzilla2
    
    * setup routing between backend and frontend
    
    * bugfix
    
    * .bat script added
    
    * added pnpm and node.js curls to .sh
    
    * minor changes .sh install script
    
    * minor changes to the .bat
    
    * add backend logic
    
    * minor changes
    
    * manually add python path before import
    
    * first draft index page
    
    * remove pnpm command from .sh
    
    * updated gitignore
    
    * added tests, cli_runner and check for biomart database
    
    * changes github workflow and minor changes
    
    * testpush
    
    * added tests, asgi, django settings and others
    
    * add pytest ini
    
    * changed pytest.ini
    
    * updated bash script to install node.js
    
    * Getting data from backend to frontend as json
    
    * WIP making tests run, unexplainable fails...
    
    * managed to rename variables to  the same value 💪
    
    * fixed most tests, WIP on test_runner
    
    * Added protzilla colours, made simple buttons with them, added gray square  + gray icon button, fixed red/redsecondary issue, fixed hover symptoms but not the cause
    
    * WIP fixed failing test, two errors
    
    * all tests running
    
    * add workflows to repo for tests, changed frontend github ci
    
    * fixed lockfile-dir, fixed coverargc
    
    * changed coverargc location, changed frontend ci
    
    * force pnpm-locl
    
    * fix frontend ci
    
    * lint comments applied
    
    * separated github workflows
    
    * Finalize setup, added dev-scripts
    
    * added requested changes
    
    * minor return type fix
    
    * run formatter
    
    * accept LF and CRLF line endings
    
    * delete node.js install script after install
    
    * minor lint fix
    
    * add formatting to frontend CI workflow
    
    * improved python import
    
    * removed json and sarahs workflows from git
    
    * update gitignore to not push workflows anymore
    
    * change location of database download
    
    * new grid system implemented in index-screen
    
    * applied changes part I
    
    * card component added
    
    * Card implemented in index-screen
    
    * minor changes on cards and switch toggle
    
    * quick index-screen fix
    
    * first modal draft
    
    * requested changes part II - added protzilla svg hehe
    
    * updated dev scripts, fixed time bug
    
    * changes for linter
    
    * initial commit + add icons
    
    * implement basic sidebar components, styling and functionality
    
    * add functionality and polish design
    
    * remove transition
    
    * fix linter
    
    * add animations and more
    
    * add handles
    
    * improve code style
    
    * fix linter
    
    * fix tests
    
    * fix test (pls)
    
    * fix tests (fr this time)
    
    * make collapse animation better
    
    * ressolve review requests
    
    * add spaces
    
    ---------
    
    Co-authored-by: sarah <sarah.vogels@aol.de>
    Co-authored-by: gitjannes <jannesk@gmx.net>
    Co-authored-by: maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
    Co-authored-by: gitjannes <131971807+gitjannes@users.noreply.github.com>
    Co-authored-by: Jonas Krohn <jonas.krohn@gmx.de>
    Co-authored-by: maximilianKalff <87859344+maximilianKalff@users.noreply.github.com>

---
+++ frontend/src/components/sidebar/sidebar.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+
+import { Sidebar } from "./sidebar"
+
+export default {
+    component: Sidebar,
+    title: "Sidebar"
+} as Meta;
+
+const Template = () => <Sidebar />;
+export const Default = Template.bind({});
