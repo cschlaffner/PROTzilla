58bcf0f8796ba6d451bb5567200ea7581e8316f3
Philipp Hendel <philipp_c@hendel.co>
2025-03-13 02:07:07 +0100

    nvm its not that simple (restore working version)

--- frontend/src/theme/styled.ts
+++ frontend/src/theme/styled.ts
@@ -1,16 +1,21 @@
 import styledComponents from 'styled-components';
 
-const isValidHtmlProp = (prop: string) => 
-    prop in document.createElement("div"); 
+// const isValidHtmlProp = (prop: string,component:string|symbol) =>  {
+//   console.log(String(component))
+//   console.log(document.createElement(String(component)))
+//   console.log(prop,prop in document.createElement(String(component)))
+//   return (prop in document.createElement(String(component))); 
+// }
+    
 
 //importing styled from here instead from "styled-components" directly makes sure only properties which React recognizes on a DOM Element are passed down
 
 
 export const styled = new Proxy(styledComponents, {
-  get(target, prop) {
+  get(target, component) {
     return (...args: any[]) =>
-      (target as any)[prop].withConfig({
-        shouldForwardProp: (prop:any) => isValidHtmlProp(prop),
+      (target as any)[component].withConfig({
+        //shouldForwardProp: (prop:string) => isValidHtmlProp(prop, component),
       })(...args);
   },
 }) as typeof styledComponents;
