10a12f17c332663601f7cdbe3f99597845546787
ronjakrg <ronja.krueger@student.hpi.de>
2025-03-23 11:44:21 +0100

    improved valueFormatter for displaying NaNs

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -1,12 +1,11 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import {
   DataGrid,
-  GridColDef,
   GridColumnVisibilityModel,
   GridPaginationModel,
   GridRowsProp,
 } from "@mui/x-data-grid";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import { DataTableProps } from "./data-table.props";
 import { getTheme } from "../../theme";
@@ -16,8 +15,7 @@ export const DataTable: React.FC<DataTableProps> = ({
   pageSize,
   pageSizeOptions,
 }) => {
-  const [rows, setRows] = useState<GridRowsProp>(data);
-  const [columns, setColumns] = useState<GridColDef[]>([]);
+  const [rows] = useState<GridRowsProp>(data);
   const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
     page: 0,
     pageSize: pageSize ?? 10,
@@ -27,32 +25,27 @@ export const DataTable: React.FC<DataTableProps> = ({
       id: false,
     });
 
-  useEffect(() => {
-    if (data.length > 0) {
-      const dynamicColumns = Object.keys(data[0]).map((key) => {
-        const isNumeric = data.every(
-          (row) => typeof row[key] === "number" || row[key] === null,
-        );
-        return {
-          field: key,
-          headerName: key,
-          minWidth: 200,
-          flex: 1,
-          type: isNumeric ? "number" : "string",
-          align: "left",
-          headerAlign: "left",
-          valueFormatter: (value: number | null) => {
-            if (value == null) {
-              return "NaN";
-            }
-            return value;
-          },
-        };
-      });
-      setColumns(dynamicColumns);
-    }
-    setRows(data);
-  }, [data]);
+    const columns = Object.keys(data[0]).map((key) => {
+      const isNumeric = data.every(
+        (row) => typeof row[key] === "number" || row[key] === null,
+      );
+      return {
+        field: key,
+        headerName: key,
+        minWidth: 200,
+        flex: 1,
+        type: isNumeric ? "number" : "string",
+        align: "left",
+        headerAlign: "left",
+        valueFormatter: (value: number | null) => {
+          if (value == null) {
+            return 'NaN';
+          }
+          return value;
+        },
+      };
+    });
+    
 
   const protzillaTheme = useMemo(() => getTheme("light"), []);
 
