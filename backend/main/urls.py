dec1a7728828bb53e881b709b062524576ba1b6d
Sarah Vogels <114360057+sarahvgls@users.noreply.github.com>
2025-03-26 14:05:45 +0100

    added component for step selection (#18)
    * starting setup of new repo
    
    * bare minimum setup to copy PROTzilla2
    
    * new structure with empty backend-frontend
    
    * minor changes
    
    * added empty react project
    
    * added react workshop structure
    
    * more workshop content and pnpm commands
    
    * added .github configurations for backend and frontend, added lint- and other precommit hooks
    
    * Update Readmes
    
    * added more setup files from PROTzilla2
    
    * setup routing between backend and frontend
    
    * bugfix
    
    * .bat script added
    
    * added pnpm and node.js curls to .sh
    
    * minor changes .sh install script
    
    * minor changes to the .bat
    
    * add backend logic
    
    * minor changes
    
    * manually add python path before import
    
    * first draft index page
    
    * remove pnpm command from .sh
    
    * updated gitignore
    
    * added tests, cli_runner and check for biomart database
    
    * changes github workflow and minor changes
    
    * testpush
    
    * added tests, asgi, django settings and others
    
    * add pytest ini
    
    * changed pytest.ini
    
    * updated bash script to install node.js
    
    * Getting data from backend to frontend as json
    
    * WIP making tests run, unexplainable fails...
    
    * managed to rename variables to  the same value 💪
    
    * fixed most tests, WIP on test_runner
    
    * WIP building api
    
    * API mostly finished (as documented in drawio), urls.py needs to be updated, file arrangement not final
    
    * Fixed some backend api endpoints, added some urls for testing, added generic api call function for frontend
    
    * some experiments with pure data getting custom hooks and functions, not final
    
    * Created paths for api endpoints, experimented with functionality
    
    * still had merge conflicts??
    
    * changes after running
    
    * Added some tests
    
    * updated gitignore
    
    * fixed a test
    
    * updated gitignore
    
    * Removed comment
    
    * remove outdated pipeline
    
    * linted
    
    * linted2: electric boogaloo
    
    * finished linting
    
    * added run data specific api endpoints and placeholders
    
    * added pure data fetching api example
    
    * WIP
    
    * WIP for step selection
    
    * preparing CORS fix
    
    * added CORS configurations (on working branch)
    
    * added CORS configurations to django settings
    
    * clean up but still WIP
    
    * styling problems
    
    * WIP step selection, changed Invisible Button inhertiance from BaseButton to Button
    
    * first version step selection & fixed close icon
    
    * working version with call to backend but cleanup needed
    
    * cleaned up code
    
    * linted
    
    * refactored code
    
    * added step navigation api endpoint
    
    * add docu
    
    * fixes after refactoring, works on 127:0.0.1
    
    * introduced useCookie hook and rearranged methods
    
    * use getCookie instead of hook, added csrf handling to api-calls
    
    * added csrf token to POST request in api-call
    
    * update django settings
    
    * add more settings
    
    * introduced button for step selection
    
    * lint partially
    
    * fix api-calls and get-cookie
    
    * updated cookies and api-calls
    
    * added explanatory comment
    
    * delete all miscellaeneos utils occurances
    
    * clean up
    
    * limit CORS_ALLOW_METHODS
    
    * cleaned up branch, mostly comments
    
    * fixed csrf handling
    
    * corrected csrf setup
    
    * minor merge issue fixed
    
    * Version 2 step selection first working
    
    * cleanup code
    
    * fixed linter issues
    
    * fix tiny bug
    
    * applied theme, damn its prettier now :D
    
    * added differnt button for comparison
    
    * resolved some merge issues, added loading info when backend not started
    
    * deleted comment
    
    * adapted changes
    
    * added boolean to control whether to open step selection via wider button with text
    
    * deleted unused api call and adapted test
    
    * removed references to get_all_possible_step_names
    
    * equal theme, minor bugfix
    
    * adapted changes
    
    ---------
    
    Co-authored-by: gitjannes <jannesk@gmx.net>
    Co-authored-by: maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
    Co-authored-by: gitjannes <131971807+gitjannes@users.noreply.github.com>

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/static/index.html')),
     path("api/get_csrf_token/", views.get_csrf_token, name="get_csrf_token"),
     path("api/run_information/", views_with_api.run_information_list, name="run_information"),
-    path("api/step_name_list/", views_with_api.step_name_list, name="step_name_list"),
+    path("api/step_list/", views_with_api.all_steps, name="step_list"),
     path("api/workflow_name_list/", views_with_api.workflow_name_list, name="workflow_name_list"),
-    
+
     path("api/toggle_favourite/", views_with_api.toggle_favourite, name="toggle_favourite"),
     path("api/add_tag/", views_with_api.add_tag, name="add_tag"),
     path("api/delete_tag/", views_with_api.delete_tag, name="delete_tag"),
@@ -46,7 +46,7 @@ urlpatterns = [
     path("api/get_step_table/", views_with_api.get_step_table, name="get_step_table"),
     path("api/get_run_data/", views_with_api.get_run_data, name="get_run_data"),
 
-    #old routes, not yet implemented as api endpints, see notion card 
+    #old routes, not yet implemented as api endpints, see notion card
     path("databases", views.databases, name="databases"),
     path("databases/upload", views.database_upload, name="database_upload"),
     path("databases/delete", views.database_delete, name="database_delete"),
