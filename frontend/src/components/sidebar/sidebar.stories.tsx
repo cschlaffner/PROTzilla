157d90ac158b9ae10e0c72ae6105fe7e5a2ff6d9
gitjannes <jannesk@gmx.net>
2025-03-31 12:05:23 +0200

    linted

--- frontend/src/components/sidebar/sidebar.stories.tsx
+++ frontend/src/components/sidebar/sidebar.stories.tsx
@@ -7,5 +7,5 @@ export default {
   title: "Sidebar",
 } as Meta;
 
-const Template = () => <Sidebar runName={""} />;
+const Template = () => <Sidebar runName={""} handleStepSelection={() => {}}/>;
 export const Default = Template.bind({});
