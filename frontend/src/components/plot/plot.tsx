773f7a9104e2b3b5287dc8ed94649e8d292abc47
ronjakrg <ronja.krueger@student.hpi.de>
2025-04-09 15:59:30 +0200

    some formatting

--- frontend/src/components/plot/plot.tsx
+++ frontend/src/components/plot/plot.tsx
@@ -3,7 +3,11 @@ import Plot from "react-plotly.js";
 
 import { PlotProps } from "./plot.props";
 
-export const PlotComponent: React.FC<PlotProps> = ({ data, layout, styleProps }) => {
+export const PlotComponent: React.FC<PlotProps> = ({
+  data,
+  layout,
+  styleProps,
+}) => {
   return (
     <div style={styleProps}>
       <Plot data={data} layout={layout} />
