6aca4c29e1e5d6fba97fbd4462179b0dfa8db218
Sarah Vogels <114360057+sarahvgls@users.noreply.github.com>
2025-04-12 11:45:13 +0200

    single checkbox field (#26)
    * single checkbox field, renamed checkbox file
    
    * linter
    
    * applied requested changes
    
    * fixed isSmall and sizing

--- frontend/src/components/input-fields/checkbox-input-fields/checkbox-select-input-field/checkbox-select-input-field.stories.tsx
+++ frontend/src/components/input-fields/checkbox-input-fields/checkbox-select-input-field/checkbox-select-input-field.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryFn } from "@storybook/react";
 
-import { CheckboxSelectInputField } from "./chechbox-select-input-field";
-import { CheckboxSelectInputFieldProps } from "./checkbox-select-input-field.props";
+import { CheckboxSelectInputFieldProps } from "./checkbox-select-input-field.props.ts";
+import { CheckboxSelectInputField } from "./checkbox-select-input-field.tsx";
 
 export default {
   component: CheckboxSelectInputField,
---
+++ frontend/src/components/input-fields/checkbox-input-fields/checkbox-select-input-field/index.ts
@@ -0,0 +1,2 @@
+export * from "./checkbox-select-input-field.tsx";
+export * from "./checkbox-select-input-field.props.ts";
