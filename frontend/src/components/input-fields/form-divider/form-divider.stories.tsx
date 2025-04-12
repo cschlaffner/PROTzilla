a46d6d38907ef1bbbdfcc37d91b9786e8d566709
Jonas Krohn <jonas.krohn@gmx.de>
2025-04-12 15:37:45 +0200

    fixing linter

--- frontend/src/components/input-fields/form-divider/form-divider.stories.tsx
+++ frontend/src/components/input-fields/form-divider/form-divider.stories.tsx
@@ -1,5 +1,5 @@
-import { Meta, StoryFn } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { Meta, StoryFn } from "@storybook/react";
 
 import { FormDivider } from "./form-divider";
 import { FormDividerProps } from "./form-divider.props";
