39329a5f9e5c81a4e389f7ba266950ece93f5dea
gitjannes <131971807+gitjannes@users.noreply.github.com>
2025-04-16 15:33:35 +0200

    Index screen (#24)
    * build first draft for index screen
    
    * update spacing
    
    * icons can now be big, buttons can now have big icons, added big button
    
    * unfinished workflows
    
    * finished workflow container
    
    * linted
    
    * looks good but has an error
    
    * runs_tables
    
    * free fix (thx jannes)
    
    * linted
    
    * added create run visual
    
    * runs table first version draft
    
    * first tags draft. (Don't worry Jannes, i will put it in an additional component)
    
    * start TagModal
    
    * removed Jannes
    
    * add Run as type
    
    * add functional index-screen
    
    * linted
    
    * made taglist a standalone component, updated the files which used the old version
    
    * changed icon for tag-modal, updated color usage in runs-table
    
    * fixed warning for text&dropdown input fields, fixed dropdown fields not displaying a default option
    
    * linted
    
    * added search for workflows and for runs
    
    * fixed dropdown options not rendering correctly
    
    * added existing tags, existing  tags can be added to run via modal,
    refactored taglist to be uniform in handling tags
    
    * fixed width bug of dropdown field and remove OLD-index-screen
    
    * linted
    
    * fixed height issue of RunSelectionCard
    
    * linted + fixed re-rendering of dropdowns
    
    * refactored tag selection menu to own component for reusability
    
    * ... and corresponding linter
    
    * runs-table 1.1 - run.run_name not working properly, Delete Modal as well
    
    * fixed deleteModal
    
    * linted
    
    * requested changes, renaming of variables, importing from theme
    
    * linted
    
    * delete modal refactored
    
    * linted
    
    * runs table scrollable & linted
    
    * action buttons cosmetics
    
    * x Direction scroll bug fixed
    
    * pnpm format
    
    * linted
    
    * fixed small scroll bug
    
    ---------
    
    Co-authored-by: Lennard <lennard.schulze@student.hpi.uni-potsdam.de>
    Co-authored-by: maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
    Co-authored-by: sarah <sarah.vogels@aol.de>

--- frontend/src/components/icon/icons/index.ts
+++ frontend/src/components/icon/icons/index.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite-plugin-svgr/client" />
 export { default as add } from "./add.svg?react";
+export { default as arrowRight } from "./arrow-right.svg?react";
 export { default as burgerMenu } from "./burger-menu.svg?react";
 export { default as checkmark } from "./checkmark.svg?react";
 export { default as chevronDoubleLeft } from "./chevron-double-left.svg?react";
@@ -22,8 +23,12 @@ export { default as incomplete } from "./incomplete.svg?react";
 export { default as info } from "./info.svg?react";
 export { default as list } from "./list.svg?react";
 export { default as outdated } from "./outdated.svg?react";
+export { default as play } from "./play-btn.svg?react";
 export { default as protzilla } from "./protzillablackwhite.svg?react";
 export { default as searchLens } from "./search-lens.svg?react";
 export { default as settings } from "./setting-fill.svg?react";
 export { default as spinner } from "./spinner.svg?react";
+export { default as star } from "./star.svg?react";
+export { default as starFill } from "./star-fill.svg?react";
+export { default as threeDots } from "./three-dots.svg?react";
 export { default as trash } from "./trash.svg?react";
