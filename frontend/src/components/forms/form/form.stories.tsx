e092f27b888442f063f6ddf518e2eb5595eb2516
Philipp Hendel <philipp_c@hendel.co>
2025-05-05 15:48:33 +0200

    fix the beloved STORYBOOK ._.

--- frontend/src/components/forms/form/form.stories.tsx
+++ frontend/src/components/forms/form/form.stories.tsx
@@ -42,14 +42,6 @@ primary.args = {
         isVisible: true,
       },
       {
-        type: "single-checkbox",
-        name: "single-checkbox",
-        props: {
-          label: "Single Checkbox",
-          text: "Checkbox",
-        },
-      },
-      {
         type: "multi-select",
         name: "country",
         label: "Country",
@@ -90,14 +82,6 @@ submit.args = {
         name: "single-checkbox",
         label: "Single Checkbox",
         isVisible: true,
-      },
-      {
-        type: "single-checkbox",
-        name: "single-checkbox",
-        props: {
-          label: "Single Checkbox",
-          text: "Checkbox",
-        },
       },
       {
         type: "multi-select",
