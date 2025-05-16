8bd1e26ec84aa052b6f72b6bda46a1f778f5187d
gitjannes <jannesk@gmx.net>
2025-05-16 18:09:50 +0200

    fix data table not being retrieved by backend, fix datatable-component  not properly updating in frontend ,add frontend handling for empty datatable

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -4,7 +4,6 @@ import {
   GridColDef,
   GridColumnVisibilityModel,
   GridPaginationModel,
-  GridRowsProp,
 } from "@mui/x-data-grid";
 import React, { useMemo, useState } from "react";
 
@@ -12,7 +11,6 @@ import { DataTableProps } from "./data-table.props";
 import { baseTheme, getMuiTheme } from "../../theme";
 
 export const DataTable: React.FC<DataTableProps> = ({ data, pageSize, pageSizeOptions }) => {
-  const [rows] = useState<GridRowsProp>(data);
   const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
     page: 0,
     pageSize: pageSize ?? 10,
@@ -46,7 +44,7 @@ export const DataTable: React.FC<DataTableProps> = ({ data, pageSize, pageSizeOp
   return (
     <ThemeProvider theme={theme}>
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         columnVisibilityModel={columnVisibilityModel}
         onColumnVisibilityModelChange={(newModel) => {
