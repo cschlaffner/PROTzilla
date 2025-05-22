44ebf4db479602a44c75215a3f4e55f4b1d8f095
Jonas Krohn <jonas.krohn@gmx.de>
2025-05-22 13:54:49 +0200

    fixing test

--- backend/tests/protzilla/test_runner.py
+++ backend/tests/protzilla/test_runner.py
@@ -111,7 +111,7 @@ def test_runner_imports(
         {'input_df': None, 'protein_group': None, 'similarity_measure': 'euclidean distance', 'similarity': 1},
         {'ttest_type': "Welch's t-Test", 'protein_df': None, 'multiple_testing_correction_method': 'Benjamini-Hochberg', 'alpha': 0.05, 'grouping': None, 'group1': None, 'group2': None},
         {'input_dict': None, 'fc_threshold': 1, 'items_of_interest': []},
-        {'proteins_df': None, 'differential_expression_threshold': 1, 'gene_sets_restring': [], 'organism': 9606, 'direction': 'both', 'background_path': None},
+        {'proteins_df': None, 'differential_expression_threshold': 0, 'gene_sets_restring': [], 'organism': 9606, 'direction': 'both', 'background_path': None},
         {'input_df_step_instance': None, 'cutoff': 0.05, 'gene_sets': ['Process', 'Component', 'Function', 'KEGG'], 'value': 'p-value', 'top_terms': 10, 'title': ''}
     ]
 
