5ce2ec3545e704931c7d5da711f5b799a6a5eed2
Philipp Hendel <philipp_c@hendel.co>
2025-05-28 16:10:57 +0200

    improve test folder structure

--- backend/tests/protzilla/test_workflow.py
+++ backend/tests/protzilla/test_workflow.py
@@ -1,16 +1,9 @@
-import pytest
-
 from unittest import mock
-from pathlib import Path
 
 from backend.protzilla.constants import paths
 from backend.protzilla.workflow import get_available_workflow_names 
 
-@pytest.fixture
-def workflows_path(request):
-    return Path(request.config.rootdir) / "/tests/test_workflows"
-
-def test_get_available_workflow_names(workflows_path):
+def test_get_available_workflow_names():
     with mock.patch.object(paths, "WORKFLOWS_PATH", paths.TEST_WORKFLOW_PATH):
         expected_files = [
             "example_workflow_short",
