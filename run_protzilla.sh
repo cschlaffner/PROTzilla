8be34967305a4f01146b5c15893174a638230005
hendraet <hendrik.raetz@niederjahna.de>
2025-09-15 13:35:03 +0200

    Minor fix in run_protzilla.sh

--- run_protzilla.sh
+++ run_protzilla.sh
@@ -8,6 +8,8 @@ echo "starting protzilla..."
 # Workaround if started non-interactively and conda is not initialized through sourcing .bashrc or .zshrc
 if ! conda --version >/dev/null; then
   eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+else
+  eval "$(conda shell.bash hook)"
 fi
 conda activate "$ENV_NAME"
 # 0.0.0.0:8000 so that it can also be exposed from docker containers
