3f171593a65ebd4017fbc84cc1c43d41316460f6
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2025-11-14 15:46:26 +0100

    improve plots saving behaviour

--- backend/protzilla/steps.py
+++ backend/protzilla/steps.py
@@ -36,6 +36,7 @@ class Step:
     method_description: str = None
     output_keys: list[str] = []
     calculation_status: Literal["complete", "outdated", "incomplete", "failed"] = "incomplete"
+    is_dumped: bool = False # Whether or not the current state of the step has been written to disk
 
     def __init__(self, instance_identifier: str | None = None):
         self.inputs: dict = {}
