3d5fbb06fc428870b42721a608641b08ebc6e1e1
hendraet <hendrik.raetz@niederjahna.de>
2025-11-21 10:58:12 +0100

    Fixes bugs in Multiflex implementation and adds more tests

--- backend/tests/protzilla/data_analysis/test_ptm_quantification.py
+++ backend/tests/protzilla/data_analysis/test_ptm_quantification.py
@@ -50,6 +50,13 @@ def get_peptide_df_AD_CTR() -> pd.DataFrame:
 
 @pytest.fixture
 def peptide_df():
+    peptide_df = get_peptide_df_AD_CTR()
+    peptide_df = peptide_df[peptide_df['Protein ID'] == 'P10636']
+    return peptide_df
+
+
+@pytest.fixture
+def peptide_df_not_all_proteins_present_in_both_conditions():
     return get_peptide_df_AD_CTR()
 
 
@@ -79,26 +86,17 @@ def metadata_df():
     return df
 
 
-# TODO: maybe remove
-# def metadata_df():
-#     dummy_protein_df = pd.DataFrame({'Sample': [
-#         'AD01_C1_INSOLUBLE_01',
-#         'AD01_C1_INSOLUBLE_02',
-#         'AD01_C1_INSOLUBLE_03',
-#     ]})  # Dummy DataFrame for metadata import
-#     df = metadata_import_method(
-#         dummy_protein_df,
-#         TEST_DATA_PATH / 'import_data/metadata/metadata_AD01.csv',
-#         feature_orientation='Columns'
-#     )['metadata_df']
-#
-#     return df
-
-
-def test_flexiquant(peptide_df, metadata_df):
-    reference_group = 'AD'
+@pytest.mark.parametrize(
+    "reference_group,grouping_column",
+    [
+        ('AD', 'Group'),
+        ('CTR', 'Group'),
+        ('C1', 'Batch'),
+        ('C2', 'Batch'),
+    ],
+)
+def test_flexiquant(peptide_df, metadata_df, reference_group, grouping_column):
     protein_group = 'P10636'
-    grouping_column = 'Group'
     num_samples = peptide_df['Sample'].nunique()
 
     mod_cutoff = 0.5
@@ -202,22 +200,7 @@ def test_flexiquant_one_sample_with_not_enough_valid_peptides(peptide_df_one_sam
                                             f"removed.")
 
 
-def test_multiflex(peptide_df, metadata_df):
-    reference_group = 'AD'
-    grouping_column = 'Group'
-    n_samples = metadata_df['Sample'].nunique()
-
-    result = multiflex_lf(
-        peptide_df=peptide_df,
-        metadata_df=metadata_df,
-        reference_group=reference_group,
-        grouping_column=grouping_column,
-        num_init=30,
-        mod_cutoff=0.5,
-        imputation_cosine_similarity=0.98,
-        deseq2_normalization=True,
-        colormap=1,
-    )
+def check_multiflex_plots_valid(result: dict, n_samples):
     assert 'plots' in result and len(result['plots']) == 3
     plots = result['plots']
     rm_score_hist_data = plots[0].data
@@ -232,6 +215,44 @@ def test_multiflex(peptide_df, metadata_df):
 
     heatmap_data = plots[2].data
     assert len(heatmap_data) == 1 and 'heatmap' in set(trace.type for trace in heatmap_data)
+
+
+@pytest.mark.parametrize(
+    "deseq2_normalization,reference_group,grouping_column,colormap",
+    [
+        (True, 'AD', 'Group', 0),
+        (False, 'AD', 'Group', 1),
+        (True, 'CTR', 'Group', 2),
+        (False, 'CTR', 'Group', 3),
+        (True, 'C1', 'Batch', 4),
+        (False, 'C1', 'Batch', 5),
+        (False, 'AD', 'Group', 6),
+        (False, 'AD', 'Group', 7),
+        (False, 'AD', 'Group', 8),
+    ],
+)
+def test_multiflex(peptide_df, metadata_df, deseq2_normalization, reference_group, grouping_column, colormap):
+    n_samples = metadata_df['Sample'].nunique()
+
+    result = multiflex_lf(
+        peptide_df=peptide_df,
+        metadata_df=metadata_df,
+        reference_group=reference_group,
+        grouping_column=grouping_column,
+        # TODO: test any of these args?
+        num_init=30,
+        mod_cutoff=0.5,
+        imputation_cosine_similarity=0.98,
+        deseq2_normalization=deseq2_normalization,
+        colormap=colormap,
+    )
+    assert (
+            'messages' in result
+            and 'skipped_proteins' in result
+            and len(result['messages']) == 0
+            and len(result['skipped_proteins']) == 0
+    )
+    check_multiflex_plots_valid(result, n_samples)
 
 
 def test_multiflex_grouping_column_not_in_df(peptide_df, metadata_df):
@@ -312,11 +333,69 @@ def test_multiflex_only_one_group(peptide_df_single_group, metadata_df):
     assert 'messages' in result
     assert result['messages'][0]['msg'] == "At least two groups are required for multiFLEX-LF analysis."
 
-# TODO:
-#  - Flexiquant produces errors and thus skips samples
-#  - No peptides present in two conditions
-#  - Imputation was unsuccessful - use old dataset
-#  - Test non_deseq2 normalization
-#  - Test cases similar to the Flexiquant tests above
-#  - for both: test other grouping column to see if there's more hard-coded stuff - make sure to use pytest option for
-#    "stratification"
+
+def test_multiflex_no_peptides_in_two_conditions(peptide_df, metadata_df):
+    reference_group = 'AD'
+    grouping_column = 'Group'
+
+    # Remove peptides in a way that half of the peptides are only in AD samples and the other half only in CTR samples
+    all_peptides = peptide_df['Sequence'].unique()
+    ad_peptides = all_peptides[ : len(all_peptides) // 2]
+    peptide_df_modified = peptide_df[
+        (peptide_df['Sample'].str.contains('AD') & peptide_df['Sequence'].isin(ad_peptides)) |
+        (~peptide_df['Sample'].str.contains('AD') & ~peptide_df['Sequence'].isin(ad_peptides))
+    ]
+
+    result = multiflex_lf(
+        peptide_df=peptide_df_modified,
+        metadata_df=metadata_df,
+        reference_group=reference_group,
+        grouping_column=grouping_column,
+        num_init=30,
+        mod_cutoff=0.5,
+        imputation_cosine_similarity=0.98,
+        deseq2_normalization=True,
+        colormap=1,
+    )
+    assert 'messages' in result and 'plots' not in result
+    assert result['messages'][0]['msg'] == ("No peptides with RM scores in at least two groups available for "
+                                            "clustering!")
+
+
+def test_multiflex_flexiquant_errors(peptide_df_not_all_proteins_present_in_both_conditions, metadata_df):
+    n_samples = metadata_df['Sample'].nunique()
+    reference_group = 'CTR'
+    grouping_column = 'Group'
+    # get all proteins that are always nan in the CTR group
+    proteins_not_in_control = (
+        peptide_df_not_all_proteins_present_in_both_conditions[
+            peptide_df_not_all_proteins_present_in_both_conditions['Sample'].str.contains('CTR')
+        ]
+        .groupby('Protein ID')
+        .filter(lambda x: x['Intensity'].isna().all())['Protein ID']
+        .unique()
+    )
+    n_proteins_not_in_control = len(proteins_not_in_control)
+
+    result = multiflex_lf(
+        peptide_df=peptide_df_not_all_proteins_present_in_both_conditions,
+        metadata_df=metadata_df,
+        reference_group=reference_group,
+        grouping_column=grouping_column,
+        num_init=30,
+        mod_cutoff=0.5,
+        imputation_cosine_similarity=0.98,
+        deseq2_normalization=False,
+        colormap=1,
+    )
+    assert (
+            'messages' in result
+            and len(result['messages']) == n_proteins_not_in_control
+            and all([p[i] in result['messages'][i] for i, p in enumerate(proteins_not_in_control)])
+    )
+    assert (
+            'skipped_proteins' in result
+            and len(result['messages']) == n_proteins_not_in_control
+            and set(result['skipped_proteins']) == set(proteins_not_in_control)
+    )
+    check_multiflex_plots_valid(result, n_samples)
