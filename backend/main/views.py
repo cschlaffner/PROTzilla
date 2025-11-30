2ebb119349b0f2e52370df677bf7f1010091dd91
3dot141592 <128993914+3dot141592@users.noreply.github.com>
2025-11-30 17:23:55 +0100

    Refactor current_step_has_plot assignment logic

--- backend/main/views.py
+++ backend/main/views.py
@@ -392,7 +392,7 @@ def get_run_data(request):
             run_data["current_section"] = run.current_step.section
             run_data["current_step_index"] = run.steps.current_step_index
             run_data["memory_usage"] = get_memory_usage()
-            run_data["current_step_has_plot"] = bool(run.current_step.plot_method)
+            run_data["current_step_has_plot"] = True if run.current_step.plot_method is not None else False
         else:
             run_data["displayed_steps"] = []
             run_data["current_section"] = None
