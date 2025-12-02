676b9f104eef72381476e26badb6be94278f6cee
Anna Polensky <anna-mail@mailbox.org>
2025-12-02 15:27:33 +0100

    feat: add import of (silac) ratios l/h

--- backend/protzilla/importing/peptide_import.py
+++ backend/protzilla/importing/peptide_import.py
@@ -5,10 +5,7 @@ import pandas as pd
 
 from backend.protzilla.importing.ms_data_import import clean_protein_groups
 
-from backend.protzilla.constants.intensitiy_types import (
-    IntensityType,
-    IntensityNameType,
-)
+from backend.protzilla.constants.intensitiy_types import IntensityType
 
 
 def peptide_import(file_path: Path, intensity_name, map_to_uniprot) -> dict:
