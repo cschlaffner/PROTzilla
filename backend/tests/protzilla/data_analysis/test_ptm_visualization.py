0d7cefd254303bb7f3c0317dc92c8a82f08a8689
hendraet <hendrik.raetz@niederjahna.de>
2026-01-02 11:31:40 +0100

    blaaaaack

--- backend/tests/protzilla/data_analysis/test_ptm_visualization.py
+++ backend/tests/protzilla/data_analysis/test_ptm_visualization.py
@@ -10,7 +10,8 @@ from protzilla.constants.paths import SETTINGS_PATH
 from protzilla.data_analysis.ptm_visualization import (
     create_overview_ptm_visualization,
     create_bar_ptm_visualization,
-    create_details_ptm_visualization, ptm_vis_utils,
+    create_details_ptm_visualization,
+    ptm_vis_utils,
 )
 from protzilla.data_analysis.ptm_visualization.ptm_overview_plot import (
     get_detected_modifications,
@@ -313,7 +314,7 @@ class TestPTMVisualization:
             mock.patch.object(
                 ptm_vis_utils,
                 "CUSTOM_PTM_SETTINGS_FILE_STEM",
-                tmp_ptm_settings_dir.resolve() / settings_reduced_ptms_file.stem
+                tmp_ptm_settings_dir.resolve() / settings_reduced_ptms_file.stem,
             ),
         ):
             result = create_overview_ptm_visualization(
