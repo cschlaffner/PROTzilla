8686b836e34a7b6433473ce8da8d1209725a536a
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-01-09 17:02:03 +0100

    Add nodes based on steps

--- frontend/src/components/app/run-screen/node-editor/node-editor.tsx
+++ frontend/src/components/app/run-screen/node-editor/node-editor.tsx
@@ -1,18 +1,17 @@
 import * as go from 'gojs';
 import { ReactDiagram } from 'gojs-react';
 
-
-
-
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import StepNode from "./StepNode.tsx";
  
-const initialNodes = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+const initialNodes = [];
+
+const initialEdges = [{ id: 'n1-n1', source: 'n1', target: 'n1' }];
+const nodeTypes = {step: StepNode};
+
+
 
 export const NodeEditor: React.FC<NodeEditorProps> = ({
   onFormSubmit,
@@ -35,12 +34,38 @@ export const NodeEditor: React.FC<NodeEditorProps> = ({
     (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
     [],
   );
+
+  useEffect(() => {
+    console.log(runData);
+    let new_nodes = [];
+    let y_offset = 0;
+
+    runData.displayed_steps.forEach(section => {
+      section.steps.forEach((step, index) => {
+        new_nodes.push({
+          id: step.id, 
+          type: "step", 
+          position: {x: 0, y: y_offset}, 
+          data: {
+            step: step, 
+            step_index_within_section: index,
+            section: section.id,
+            navigateOrRefreshSteps: navigateOrRefreshSteps,
+          }
+        });
+
+        y_offset += 40;
+      });
+    });
+    setNodes(new_nodes);
+  }, [runData])
  
   return (
     <div style={{ width: '25vw', height: '100vh' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
+        nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
