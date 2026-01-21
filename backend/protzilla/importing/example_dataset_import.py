79a669d57d02138b40b4eb5f8282d971e4d3f455
hendraet <hendrik.raetz@niederjahna.de>
2026-01-21 17:27:47 +0100

    WIP: Adds code to download stuff from pride

--- backend/protzilla/importing/example_dataset_import.py
+++ backend/protzilla/importing/example_dataset_import.py
@@ -1,16 +1,64 @@
+import logging
+from pathlib import Path
+
+from pridepy import pridepy
+
+from protzilla.constants.intensity_types import IntensityType
 from protzilla.constants.paths import (
     EXAMPLE_DATASET_PROTEIN_FILE,
     EXAMPLE_DATASET_METADATA_FILE,
     EXAMPLE_DATASET_EVIDENCE_FILE,
+    EXAMPLE_DATASET_DIR,
 )
 from protzilla.importing.import_utils import FeatureOrientationType
-from protzilla.constants.intensity_types import IntensityType
 from protzilla.importing.metadata_import import metadata_import_method
 from protzilla.importing.ms_data_import import max_quant_import
 from protzilla.importing.peptide_import import evidence_import
 
 
+# TODO: maybe update dependencies if we need pridepy
+
+
 def example_dataset_import():
+    # TODO: remove - only testing
+    # accession = "PXD020517"
+    # filename = "AD02_BA39-Cohort1_INSOLUBLE_03.wiff"
+    accession = "PXD014997"
+    filename = "SEARCH-IDENTIFICATION_REL-FREEvsRELAPSE_Label-free.7z"
+    if (
+        not EXAMPLE_DATASET_PROTEIN_FILE.exists()
+        or not EXAMPLE_DATASET_METADATA_FILE.exists()
+        or not EXAMPLE_DATASET_EVIDENCE_FILE.exists()
+        or not Path(EXAMPLE_DATASET_DIR, filename).exists()
+    ):
+
+        # TODO: remove
+        # use normal logger for now
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        logger.info(f"Downloading file {filename} from PRIDE project {accession}")
+
+        raw_files = pridepy.Files()
+        raw_files.download_file_by_name(
+            accession=accession,
+            file_name=filename,
+            output_folder=str(EXAMPLE_DATASET_DIR),
+            skip_if_downloaded_already=True,
+            protocol="ftp",
+            username=None,
+            password=None,
+            aspera_maximum_bandwidth=None,
+            checksum_check=False,
+        )
+        print("Download completed")  # TODO: remove
+        return
+
+    # import py7zr  # TODO: install
+    #
+    # with py7zr.SevenZipFile('sample.7z', mode='r') as z:
+    #     z.extractall()
+
     intensity_name = IntensityType.LFQ_INTENSITY.value
     protein_import_dict = max_quant_import(
         file_path=EXAMPLE_DATASET_PROTEIN_FILE,
@@ -44,3 +92,12 @@ def example_dataset_import():
     }
 
     return combined_dict
+
+
+# TODO: remove
+def main():
+    result = example_dataset_import()
+
+
+if __name__ == "__main__":
+    main()
