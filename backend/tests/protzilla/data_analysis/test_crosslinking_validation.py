3ff37f6d36e0c4e4f764cdbb1709e78ae2803f69
Anna Polensky <anna-mail@mailbox.org>
2026-01-31 16:42:20 +0100

    test: add more tests for crosslinking validation

--- backend/tests/protzilla/data_analysis/test_crosslinking_validation.py
+++ backend/tests/protzilla/data_analysis/test_crosslinking_validation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 from unittest.mock import patch
 from unittest.mock import MagicMock
 
@@ -6,6 +7,7 @@ from unittest.mock import MagicMock
 from backend.protzilla.data_analysis.crosslinking_validation import (
     get_position_of_amino_acid_crosslinker_bound_to,
     validate_with_angstrom_deviation,
+    get_distance_between_two_amino_acids_in_angstrom,
 )
 from protzilla.methods.data_analysis import CrossLinkingValidationWithAngstromDeviation
 
@@ -20,13 +22,22 @@ def test_get_position_of_amino_acid_crosslinker_bound_to():
 @patch(
     "backend.protzilla.data_analysis.crosslinking_validation.fetch_alphafold_protein_structure"
 )
-def test_validate_with_angstrom_deviation(mock_fetch):
+@pytest.mark.parametrize(
+    "distance, expected",
+    [
+        (3.99, False),  # outside bounds
+        (4.0, True),  # lower bound
+        (6.0, True),  # upper bound
+        (6.01, False),  # outside bounds
+    ],
+)
+def test_validate_with_angstrom_deviation(mock_fetch, distance, expected):
     # Fake AlphaFold Data
     cif_df = pd.DataFrame(
         {
             "_atom_site.label_atom_id": ["CA", "CA"],
             "_atom_site.label_seq_id": [1, 2],
-            "_atom_site.Cartn_x": [0, 4],
+            "_atom_site.Cartn_x": [0, distance],
             "_atom_site.Cartn_y": [0, 0],
             "_atom_site.Cartn_z": [0, 0],
         }
@@ -61,8 +72,8 @@ def test_validate_with_angstrom_deviation(mock_fetch):
 
     assert "alphafold_distance" in df.columns
     assert "valid_crosslink" in df.columns
-    assert df.loc[0, "alphafold_distance"] == 4.0
-    assert df.loc[0, "valid_crosslink"] is True
+    assert df.loc[0, "alphafold_distance"] == distance
+    assert df.loc[0, "valid_crosslink"] is expected
 
 
 def test_modify_form_creates_crosslinker_fields():
@@ -86,3 +97,19 @@ def test_modify_form_creates_crosslinker_fields():
     assert "BS3_length" in form
     assert "BS3_upper_accepted_deviation" in form
     assert "BS3_lower_accepted_deviation" in form
+
+
+def test_get_distance_between_two_amino_acids_in_angstrom():
+    cif_df = pd.DataFrame(
+        {
+            "_atom_site.label_atom_id": ["CA", "CA"],
+            "_atom_site.label_seq_id": [1, 2],
+            "_atom_site.Cartn_x": [0, 3],
+            "_atom_site.Cartn_y": [0, 4],
+            "_atom_site.Cartn_z": [0, 0],
+        }
+    )
+
+    dist = get_distance_between_two_amino_acids_in_angstrom(1, 2, "A", "B", cif_df)
+
+    assert dist == 5.0
