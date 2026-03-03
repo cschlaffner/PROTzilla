be0e12b55c88499e3d81fe636abda5ba3c3df4d9
Tarek Massini <tarek@massini.me>
2026-03-03 05:11:18 +0100

    fix: pinky promise last importing fix

--- backend/protzilla/disk_operator.py
+++ backend/protzilla/disk_operator.py
@@ -12,7 +12,7 @@ import yaml
 from plotly.io import read_json, write_json
 
 from backend.protzilla.constants.data_types import DataKey
-import backend.protzilla.utilities as utilities
+import backend.protzilla.utilities.utilities as utilities
 from backend.protzilla.constants import paths
 from backend.protzilla.constants.date_format import metadata_date_format
 from backend.protzilla.constants.protzilla_logging import logger
