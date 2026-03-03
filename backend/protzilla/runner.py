20f854ea55905136d89331fdb60a99f70da711b2
3dot141592 <robin.luepke@gmail.com>
2026-03-03 04:30:16 +0100

    plot path uses step id

--- backend/protzilla/runner.py
+++ backend/protzilla/runner.py
@@ -129,7 +129,7 @@ class Runner:
 
     def _save_plots_html(self, step):
         for i, plot in enumerate(step.plots):
-            plot_path = f"{self.plots_path}/{self.run.steps.current_step_id}-{step.section}-{step.operation}-{step.display_name}-{i}.html"
+            plot_path = f"{self.plots_path}/{step.instance_identifier}-{step.section}-{step.operation}-{step.display_name}-{i}.html"
             plot.write_html(plot_path)
 
     def _overwrite_run_prompt(self):
