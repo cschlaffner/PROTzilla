12d9458edad92130d8c61318b38ca2f228b86815
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-03-06 16:13:56 +0100

    remove list editor

--- frontend/src/components/app/run-screen/sidebar/sidebar.props.ts
+++
@@ -1,9 +0,0 @@
-import { RunData, Section, StepID } from "@protzilla/utils";
-
-export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  runName: string;
-  runData: RunData;
-  sections: Section[];
-  stepSectionIndex: number | undefined;
-  navigateOrRefreshSteps: (stepID?: StepID) => void;
-}
