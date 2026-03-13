71c8147587b29ac13700637a60f8765a19745052
Elena-kal <kalbitzer.elena@gmail.com>
2026-03-13 12:41:01 +0100

    fix: change input data of ModelEvaluator to protein_df and metadata_df in order to only use input data that users can upload in protzilla

--- backend/protzilla/data_analysis/model_evaluation.py
+++ backend/protzilla/data_analysis/model_evaluation.py
@@ -6,28 +6,29 @@ from backend.protzilla.data_analysis.classification_helper import (
 )
 
 
-def evaluate_classification_model(model, input_test_df, labels_test_df, scoring):
+def evaluate_classification_model(model, protein_df, metadata_df, scoring):
     """
     Function that asseses an already trained classification model on separate testing
     data using widely used scoring metrics
 
     :param model: The trained classification model instance to be evaluated.
     :type model: BaseEstimator
-    :param input_test_df: The input features of the testing data as a DataFrame.
-    :type input_test_df: pd.DataFrame
-    :param labels_test_df: The true labels of the testing data as a DataFrame.
-    :type labels_test_df: pd.DataFrame
+    :param protein_df: The input features of the testing data as a DataFrame.
+    :type protein_df: pd.DataFrame
+    :param metadata_df: The true labels of the testing data as a DataFrame.
+    :type metadata_df: pd.DataFrame
     :param scoring: The scoring metric to be used for evaluation. It can be a string
         representing a predefined metric e.g. accuracy, precision, recall, matthews_corrcoef
     :type scoring: str or callable
     :return: A dataframe with the metric name and its corresponding score.
     :rtype: dict
     """
-    input_test_df = input_test_df.set_index("Sample")
-    _, labels_test_df = encode_labels(labels_test_df, "Label")
+    protein_df = protein_df.set_index("Sample")
+    metadata_df = metadata_df.set_index("Sample")
+    _, metadata_df = encode_labels(metadata_df, "Group")
 
-    y_pred = model.predict(input_test_df)
-    scores = evaluate_with_scoring(scoring, labels_test_df["Encoded Label"], y_pred)
+    y_pred = model.predict(protein_df)
+    scores = evaluate_with_scoring(scoring, metadata_df["Encoded Label"], y_pred)
 
     scores_df = pd.DataFrame.from_dict(scores, orient="index", columns=["Score"])
     scores_df = scores_df.reset_index().rename(columns={"index": "Metric"})
