b5957fd12aa29c9843ba6778d8b7bdf2c5a915e9
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-03-17 14:31:25 +0100

    black

--- backend/protzilla/data_analysis/plots.py
+++ backend/protzilla/data_analysis/plots.py
@@ -210,7 +210,15 @@ def create_volcano_plot(
         selector=dict(name=f"Not Significant {item_type}s"),
     )
 
-    return dict(plots=[fig], messages=[dict(level= logging.INFO, msg=f"Using possibly corrected alpha with value of {alpha}")])
+    return dict(
+        plots=[fig],
+        messages=[
+            dict(
+                level=logging.INFO,
+                msg=f"Using possibly corrected alpha with value of {alpha}",
+            )
+        ],
+    )
 
 
 def clustergram_plot(
