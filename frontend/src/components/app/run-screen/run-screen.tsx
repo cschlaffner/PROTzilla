624c357b42663035fb70ff8c51c50afaaaad8a2a
Nele Riediger <75492653+NeleRiediger@users.noreply.github.com>
2026-03-18 15:55:12 +0100

    fix: footer messages to always be displayed at the bottom of the screen

--- frontend/src/components/app/run-screen/run-screen.tsx
+++ frontend/src/components/app/run-screen/run-screen.tsx
@@ -43,6 +43,9 @@ const StyledCardRow = styled(FlexRow)`
 
 const StyledFlexColumn = styled(FlexColumn)`
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const StyledCol = styled(Col)`
@@ -376,7 +379,9 @@ export const RunScreen: React.FC = () => {
                 hasCardTitle={false}
               />
             </StyledCol>
-          ) : null}
+          ) : (
+            <div style={{ flex: 1 }} />
+          )}
           <FooterText>{getFooterMessage()}</FooterText>
         </StyledFlexColumn>
       </StyledCardRow>
