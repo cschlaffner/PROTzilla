7059ff6710f30929fd66ae318075773956c86f1f
3dot141592 <robin.luepke@gmail.com>
2026-03-23 13:06:15 +0100

    also remove import-mapping-file when removing workflow

--- backend/protzilla/workflow.py
+++ backend/protzilla/workflow.py
@@ -15,8 +15,11 @@ def get_available_workflow_names() -> list[str]:
 
 def delete_workflow_file(name: str) -> tuple[bool, str]:
     workflow_path = paths.WORKFLOWS_PATH / f"{name}.yaml"
+    file_input_map_path = paths.WORKFLOWS_PATH / f"{name}.file_input_map.yaml"
     if workflow_path.exists():
         workflow_path.unlink()
+        if file_input_map_path.exists():
+            file_input_map_path.unlink()
         return (True, "")
     else:
         return (False, workflow_path)
