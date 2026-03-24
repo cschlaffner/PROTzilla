5aab6f62b63d40b5d52d2b34ca8df3753eaffba8
Hendrik Raetz <hendraet@users.noreply.github.com>
2026-03-24 13:01:58 +0100

    Improve ptm visualization (again) (#228)
    * Remove lfs files
    
    * WIP: Adds code to download stuff from pride
    
    * Finishes example dataset download via PRIDE
    
    * Changes pridepy requirements after dependency changes
    
    * Changes example import so that it imports the correct SILAC data and enables evidence import to support SILAC ratios
    
    * Enables selective import of evidence file and adds tests
    
    * WIP: adds proper metadata support for PTM visualizations
    
    * Replaces group file with metadata and adapts test
    
    * Minor fixes, more tests and clean up
    
    * Adapt test cases to catch wrong plots better
    
    * Improves PTM visualization testing
    
    * Slightly improves PTM vis tests
    
    * Improves tests for PTM visualization
    
    * Fixes and improves test for PTM vis
    
    * Further improves ptm visualization tests
    
    * WIP: Tries to improve tests and starts to refactor major parts of the code
    
    * black
    
    * Fixes empty regions labeled as nan
    
    * Revert "Fixes empty regions labeled as nan"
    
    This reverts commit b20f8aa30a6b4c33ed1e16fccd4f6c1cfe6c3b48.
    
    * Revert "black"
    
    This reverts commit c28f49b36832d26a8a23058112294423f1ed760d.
    
    * Revert "WIP: Tries to improve tests and starts to refactor major parts of the code"
    
    This reverts commit d15c226ea7c1c8df8c1eec32dcde774e70ac901d.
    
    * Changes test case slightly
    
    * Adds extensive test case and stronger check that PTM labels are within bounds
    
    * Finishes tests for single fasta and adpats old ones so that they account for the fixed filtering but
    
    * Ensures that groups stay in the same order as in metadata + cosmetic changes
    
    * Adds test stub for overly large figures
    
    * Code formatting
    
    * Cleanup
    
    * Adapts PTM vis to graph-based workflow and minor cleanup
    
    * Fixes tests
    
    * fix some tests
    
    * black
    
    ---------
    
    Co-authored-by: 3dot141592 <128993914+3dot141592@users.noreply.github.com>
    Co-authored-by: jorisfu <joris.funk@student.hpi.uni-potsdam.de>

--- backend/tests/protzilla/data_analysis/test_protein_coverage.py
+++ backend/tests/protzilla/data_analysis/test_protein_coverage.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import pytest
 
-from backend.protzilla.constants.intensity_types import IntensityType
 from backend.protzilla.constants.data_types import DataKey
+from backend.protzilla.constants.intensity_types import IntensityType
 from backend.protzilla.data_analysis.protein_coverage import (
     distribute_to_rows,
     PeptideMatch,
