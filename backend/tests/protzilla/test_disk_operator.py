ab6c162097b1d2be1883ee922f80ab043ce5d7b5
hendraet <hendrik.raetz@niederjahna.de>
2026-03-26 12:39:17 +0100

    black

--- backend/tests/protzilla/test_disk_operator.py
+++ backend/tests/protzilla/test_disk_operator.py
@@ -18,11 +18,11 @@ def test_yaml_operator_path_handling(tmp_output_dir):
     # explicitly try to create a WindowsPath on Linux/Mac or a PosixPath on Windows.
     yaml_operator = YamlOperator()
     dummy_path = "/backend/user_data/runs/dummy/run.yaml"
-    data_dict = {
-        "file_path": pathlib.Path(dummy_path)
-    }
+    data_dict = {"file_path": pathlib.Path(dummy_path)}
     out_file_path = tmp_output_dir / "yaml_test.yaml"
     yaml_operator.write(out_file_path, data_dict)
     yaml_file_contents = yaml_operator.read(out_file_path)
     assert yaml_file_contents["file_path"] == pathlib.Path(dummy_path)
-    assert str(yaml_file_contents["file_path"]) == str(pathlib.PureWindowsPath(dummy_path))
+    assert str(yaml_file_contents["file_path"]) == str(
+        pathlib.PureWindowsPath(dummy_path)
+    )
