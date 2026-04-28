1b14e704e715d5ca59954b15a015f6048cc6fc95
Note <kunaphas.kon@gmail.com>
2026-04-28 15:24:04 +0200

    remove previously preprocessed .dropna() code

--- backend/protzilla/data_analysis/differential_expression_t_test.py
+++ backend/protzilla/data_analysis/differential_expression_t_test.py
@@ -123,12 +123,6 @@ def t_test(
             intensity_name
         ]
 
-        # group1_intensities = group1_intensities.dropna()
-        # group2_intensities = group2_intensities.dropna()
-        # if len(group1_intensities) < 2 or len(group2_intensities) < 2:
-        #    if not exists_message(messages, INVALID_PROTEINGROUP_DATA_MSG):
-        #        messages.append(INVALID_PROTEINGROUP_DATA_MSG)
-        #    continue
 
         t, p = stats.ttest_ind(
             group1_intensities,
