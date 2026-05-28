0cb9ec0da27a5ee719a27b24d87b7855389a02e9
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-05-28 10:12:51 +0200

    chore: black

--- backend/tests/protzilla/importing/test_alphafold_protein_structure_load.py
+++ backend/tests/protzilla/importing/test_alphafold_protein_structure_load.py
@@ -505,7 +505,9 @@ def test_upload_multimer_prediction_basic(tmp_path, monkeypatch):
         '{"chain_iptm": [0.42, 0.89]}'
     )  # Note that we do not use these metrics anywhere
     full = tmp_path / "full.json"
-    full.write_text('{"random_column": [1,2], "pae": [[1, 2], [3, 4]]}, "token_res_ids": [1, 2, 1]')
+    full.write_text(
+        '{"random_column": [1,2], "pae": [[1, 2], [3, 4]]}, "token_res_ids": [1, 2, 1]'
+    )
     job_request = tmp_path / "job_request.json"
     job_request.write_text(
         json.dumps(
@@ -1060,7 +1062,9 @@ N N SER
     full_data = prot_dir / "full.json"
     job_request = prot_dir / "job_request.json"
     confidence.write_text(json.dumps({"chain_iptm": [0.75]}))
-    full_data.write_text(json.dumps({"pae": [[0.1, 0.2], [0.3, 0.4]], "token_res_ids": [1]}))
+    full_data.write_text(
+        json.dumps({"pae": [[0.1, 0.2], [0.3, 0.4]], "token_res_ids": [1]})
+    )
     job_request.write_text(
         json.dumps(
             [
